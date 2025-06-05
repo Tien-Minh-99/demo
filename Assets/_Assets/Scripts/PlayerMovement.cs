d1d6ea7188699cfef1599b2ea276c9d2975dba11
Tien-Minh-99 <sherlockminh1999@gmail.com>
2025-06-05 21:17:02 +0700

    demo1

---
+++ Assets/_Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //var: thay vì viết kiểu dữ liệu thì var sẽ lấy kết quả trả về làm kiểu dữ liệu 
+        //screen là màn hình 
+        // World là màn hình scene
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+    }
+}
